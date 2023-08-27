a15aa97f52da845ba78f25652fe433ccef9d4063
westhecool <52859033+westhecool@users.noreply.github.com>
2023-08-26 22:42:31 -0400

    fix some things

--- init.sh
+++ init.sh
@@ -9,4 +9,4 @@ fi
 find /proc/sys/fs/binfmt_misc -type f -name 'qemu-*' -exec sh -c 'echo -1 > {}' \;
 sleep 1
 bash /qemu-binfmt-conf.sh --qemu-suffix "-static" --qemu-path "/qemu"
-bash /run.sh $@
+bash /run.sh "$@"
